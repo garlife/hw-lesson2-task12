f62a79c983f3ccd43d3ba23149ae9ac137f3d86e
garlife <garlife@gmail.com>
2021-02-17 19:42:16 +0300

    end1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlackComponent } from './black/black.component';
+import { WhiteComponent } from './white/white.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlackComponent,
+    WhiteComponent
   ],
   imports: [
     BrowserModule,
